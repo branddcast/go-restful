13b745f071b818255f9f55c39861937ca145a27b
Brandon Jaime <brandon.jaime.castillo@banorte.com>
2021-08-19 12:51:59 -0500

    add dockerfile

--- database/models.sql
+++ database/models.sql
@@ -2,24 +2,6 @@
 
 DROP TABLE IF EXISTS articles;
 
-DO $$
-BEGIN
-CREATE ROLE gouser WITH LOGIN PASSWORD 'gouser';
-EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-END
-$$;
-
--- DO $$
--- BEGIN
--- CREATE DATABASE go_restful OWNER gouser;
--- EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
--- END
--- $$;
-
-GRANT USAGE ON SCHEMA go_restful TO gouser;
-
---- CREATE DATABASE IF NOT EXISTS go_restful OWNER gouser;
-
 CREATE TABLE articles (
     id serial NOT NULL,
     title VARCHAR(150) NOT NULL,
@@ -27,8 +9,6 @@ CREATE TABLE articles (
     content VARCHAR(500) NOT NULL UNIQUE,
     CONSTRAINT pk_articles PRIMARY KEY(id)
 );
-
-ALTER TABLE articles OWNER TO gouser;
 
 INSERT INTO articles VALUES (1, 'Articulo 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.');
 INSERT INTO articles VALUES (2, 'Articulo 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.');
